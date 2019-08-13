9905881e47c3297995402d78f5ec7cc7d428340f
tostre <marcel.mueller3101@gmail.com>
2019-08-13 14:50:25 +0200

    first pytorch tests

---
+++ modules/support/touch_test.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.optim as optim
+
+
+# Wenn ich hier ein NN aufbauen will, muss die Klasse von nn.Module erben
+# die methoden forward und num_flat_features kommen von der vererbung und müssen implementiert werden
+class Torch_Test(nn.Module):
+    def __init__(self):
+        super(Torch_Test, self).__init__()
+
+        # Ein linearer Layer im NN
+        # 10 = Eingabefeatures, 10 = Ausgabefeatures (features = neuronen)
+        self.lin1 = nn.Linear(10, 10)
+        # Diese Schicht muss die gleichen Anzahl INputfeatures haben, wie die letzt OUtput-Features
+        self.lin2 = nn.Linear(10, 10)
+
+    # nimmt den input x und rechnet ihn einmal komplett durch das netz
+    # implementiert die forward propagation
+    def forward(self, x):
+        # man weist dem Layer eins die Aktivierungsfunktion relu zu
+        # In x wird die Ausgabe von lin1 mit der Aktobierungsfunktion relu gespeichert
+        x = F.relu(self.lin1(x))
+        # Die letzte (Ausgabeschicht) hat keine Aktivuerzungfsfunktion
+        x = self.lin2(x)
+        return x
+
+
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        num = 1
+        for i in size:
+            num *= i
+        return num
+
+
+netz = Torch_Test()
+netz.cuda()
+#print(netz)
+
+# Variable baut aus einem Tensor eine variable
+# DIe lin1/lin2 erwarten eine Eingabegröße von 10 (d.h. ein Vektor). Man erstellt aber eine Matrix mit 10*10
+# Torch macht es dann automatisch so, dass es die Matrix zeilenweise nummt und alle Zeilen nacheinander ins Netz einspeist
+for i in range(100):
+    x = [1,0,0,1,0,1,0,1,1,0]
+    input = Variable(torch.Tensor([x for _ in range(10)]))
+    input = input.cuda()
+    out = netz(input)
+    #print(out)
+
+    # Das ist der Zielvektor. Er enthält die gewünschten Werte für die Klassifikation (die label)
+    # Ein WErt enstpricht hier einer Zeile in der input-Matrix
+    # Das Netz soll das Label berechnet für eine EIngabe von 10 Features (WErten/NEuronen)
+    target_list = [0,1,1,0,1,0,1,0,0,1]
+    target = Variable(torch.Tensor([target_list for _ in range(10)]))
+    target = target.cuda()
+    # Wie soll der Fehler berechnet werden? (mean squared error)
+    criterion = nn.MSELoss()
+    # Hier wird der Fehler einer Eingabe und seines Targets berechnet
+    loss = criterion(out, target)
+    print(loss)
+    # Gracienten auf 0 setzen, weil da noch werte aus dem vorigen trainingsschritt drin sein können
+    netz.zero_grad()
+    # loss durch backpropagation durchlaufen lassen
+    loss.backward()
+    # optimizer mit stochastic gradient descent
+    # parameter aus dem netz, lr=learning rate
+    optimizer = optim.SGD(netz.parameters(), lr=0.05)
+    optimizer.step()
--- modules/test_tensor.py
+++ modules/test_tensor.py
@@ -3,27 +3,51 @@ import functools
 import numpy as np
 import tensorflow as tf
 import pandas
+import torch
+import torch.nn as nn
 
 class TF:
     def __init__(self):
+        # Ausgabe von Panda-Dataframes nicht verkürzen
+        pandas.set_option('display.max_rows', 500)
+        pandas.set_option('display.max_columns', 500)
+        pandas.set_option('display.width', 1000)
+
         self.categories = ["happiness", "sadness", "anger", "fear", "disgust"]
 
         # ZTum Trainieren muss man die Label (categories in Zahlen umwandeln. Am einfachsten ist es den Index in der LIste zu nehmen:
-        for category in self.categories:
-            category_num = self.categories.index(category)
-            print(category, category_num)
+        # for category in self.categories:
+        #     category_num = self.categories.index(category)
+         #   print(category, category_num)
 
-        # Data sets
-        IRIS_TRAINING = "iris_training.csv"
-        IRIS_TEST = "iris_test.csv"
+    def erstes_netz(self):
+        pass
 
+    def rumprobieren(self):
+            # Matrix mit 5 Zeilen und 3 Spalten
+            a = torch.randn(2, 3)
+            b = torch.rand(2, 3)
+            # Fragen ob Cuda da ist, dann die Befehle unten auführen
+            # Dann werden die Berechnungen dieser Tensoren auf der GraKa ausgeführt
+            if torch.cuda.is_available():
+                print("GraKa is available")
+                a = a.cuda()
+                b = b.cuda()
+            # Matrizen einfach addieren
+            z = torch.add(a, b)
+            print("a+b", z)
 
-        self.training_data = pandas.read_csv("datasets/lexicon.csv", delimiter=",",
-                        dtype={"text": str, "affect": str, "embeddings": object,
-                               "lemmas": object})
-        self.test_data = pandas.read_csv("datasets/lexicon_test.csv", delimiter=",",
-                                             dtype={"text": str, "affect": str, "embeddings": object,
-                                                    "lemmas": object})
+            # Tensor: Vektor
+            c = torch.randn(5)
+            print(c)
+            # Tensor: 3D-Matrix
+            d = torch.randn(5, 2, 2)
+            print(d)
+            print(d.size())
+            # Tensoren mit Listen erstellen
+            e = torch.Tensor([[1, 2, 3, 4, 5], [3, 5, 7, 3, 6]])
+            print(e)
+
 
 
 tf_test = TF()
